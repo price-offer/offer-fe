91a1f2289d3576a06684b8080aa10c4ed27b5701
shinhyojeong <hyojeonge0@naver.com>
2022-08-05 01:44:55 +0900

    🔧 config: storybook alias 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ["../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -8,5 +10,14 @@ module.exports = {
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
   },
 };

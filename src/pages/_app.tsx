b5f194c480967a11747c7009f912bfe5d139c81e
shinhyojeong <hyojeonge0@naver.com>
2022-08-07 23:30:40 +0900

    🔧 config: lint 변경 및 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
+import type { ReactElement } from 'react'
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }: AppProps): ReactElement => {
   return <Component {...pageProps} />
 }
 

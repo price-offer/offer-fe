2c1459e5c67cdacd44c7e67322904e9c695ae882
shinhyojeong <hyojeonge0@naver.com>
2022-08-18 13:56:57 +0900

    ✨ feat: radius theme 추가

---
+++ src/themes/radius.ts
@@ -0,0 +1,12 @@
+import type { ThemeItem } from '@types'
+
+type RadiusKeys = 'round04' | 'round08' | 'round12' | 'round16'
+
+export type Radius = ThemeItem<RadiusKeys>
+
+export const radius: Radius = {
+  round04: '4px',
+  round08: '8px',
+  round12: '12px',
+  round16: '16px'
+}
--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -1,8 +1,9 @@
 import '@emotion/react'
-import type { Colors } from '@themes'
+import type { Colors, Radius } from '@themes'
 
 declare module '@emotion/react' {
   export interface Theme {
     colors: Colors
+    radius: Radius
   }
 }

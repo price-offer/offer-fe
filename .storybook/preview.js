39b63981b4cb82ab153ff823701a0c29a0c38da3
sonsurim <surim014@naver.com>
2022-08-22 01:57:52 +0900

    Squashed commit of the following:  -   config: storybook theme 설정 (#13)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import { theme } from '@themes'
+import { ThemeProvider } from "@emotion/react"
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +10,11 @@ export const parameters = {
     }
   }
 }
+
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={theme}>
+      <Story/>
+    </ThemeProvider>
+  )
+]

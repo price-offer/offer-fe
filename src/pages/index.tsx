08cac309d46d52f05385068a47bdbdece01ab9d6
shinhyojeong <hyojeonge0@naver.com>
2022-08-21 23:43:46 +0900

    ✨feat: img, icon 파일 경로 변경 및 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+// import * as imag from '/public/assets/images/category_appliance.png'
+import { IMAGE } from '@constants'
+import Image from 'next/image'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div></div>
+  return <Image alt="imag" src={IMAGE.CATEGORY_CAR} />
 }
 
 export default Home

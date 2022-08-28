9103ff2337c308769f076eb28bf26a7b83bcbe83
chojooyoung <jzo09@naver.com>
2022-08-29 01:04:06 +0900

    ♻️ refactor: 규칙에 맞게 styled component 수정

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,26 +1,32 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { Arrow, ArrowBox, Image, ImageBox, Slider } from './style'
+import {
+  StyledArrow,
+  StyledArrowBox,
+  StyledCarouselContainer,
+  StyledCurrentDot,
+  StyledDot,
+  StyledDotBox,
+  StyledImage,
+  StyledImageBox,
+  StyledSlider
+} from './style'
 import { useEffect, useState } from 'react'
 import { ICON } from '../../constants/icons'
-import styled from '@emotion/styled'
 
 interface CarouselProps {
   images: { url: string; id: number }[]
   isArrow: boolean
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const Carousel = ({ images, isArrow }: CarouselProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const arrowRight = ICON.CHEVRON_RIGHT_40
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const arrowLeft = ICON.CHEVRON_LEFT_40
   const [translateValue, setTranslateValue] = useState<number>(0)
   const [mouseDownClientX, setMouseDownClientX] = useState<number>(0)
   const [mouseUpClientX, setMouseUpClientX] = useState<number>(0)
   const [cursorOn, setCursorOn] = useState<boolean>(false)
 
-  const moveCurrenyy = (imageIndex: number) => {
+  const moveCurrent = (imageIndex: number) => {
     setTranslateValue((imageIndex - 1) * 60)
   }
   const moveRight = (): void => {
@@ -35,18 +41,25 @@ const Carousel = ({ images, isArrow }: CarouselProps) => {
     if (translateValue !== 0) {
       setTranslateValue(prev => prev - 60)
     } else {
-      setTranslateValue(70 * (images.length - 1))
+      setTranslateValue(60 * (images.length - 1))
     }
   }
 
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   const onMouseDown = (e: any) => {
     setMouseDownClientX(e.clientX)
     setCursorOn(true)
   }
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+
   const onMouseUp = (e: any) => {
     setMouseUpClientX(e.clientX)
+    setCursorOn(false)
+  }
+  const handleTouchStart = (e: any) => {
+    setMouseUpClientX(e.touches[0].clientX)
+    setCursorOn(true)
+  }
+  const handleTouchStartEnd = (e: any) => {
+    setMouseDownClientX(e.changedTouches[0].clientX)
     setCursorOn(false)
   }
 
@@ -69,81 +82,47 @@ const Carousel = ({ images, isArrow }: CarouselProps) => {
 
   return (
     // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    <Container>
-      <Slider
+    <StyledCarouselContainer>
+      <StyledSlider
         cursorOn={cursorOn}
         onMouseDown={onMouseDown}
-        onMouseUp={onMouseUp}>
-        <ImageBox translateValue={translateValue !== 0 ? translateValue : null}>
+        onMouseUp={onMouseUp}
+        onTouchEnd={handleTouchStartEnd}
+        onTouchStart={handleTouchStart}>
+        <StyledImageBox
+          translateValue={translateValue !== 0 ? translateValue : null}>
           {images.map((image, idx) => {
-            return <Image key={image.id} alt={'image' + idx} src={image.url} />
+            return (
+              <StyledImage key={image.id} alt={'image' + idx} src={image.url} />
+            )
           })}
-        </ImageBox>
+        </StyledImageBox>
         {isArrow && (
-          <ArrowBox>
-            <Arrow src={arrowLeft.src} onClick={moveLeft}></Arrow>
-            <Arrow src={arrowRight.src} onClick={moveRight}></Arrow>
-          </ArrowBox>
+          <StyledArrowBox>
+            <StyledArrow src={arrowLeft.src} onClick={moveLeft}></StyledArrow>
+            <StyledArrow src={arrowRight.src} onClick={moveRight}></StyledArrow>
+          </StyledArrowBox>
         )}
-      </Slider>
-      <DotBox>
+      </StyledSlider>
+      <StyledDotBox>
         {images.map(image => {
           // eslint-disable-next-line @typescript-eslint/no-use-before-define
           return (
-            <Dot
+            <StyledDot
               key={image.id}
               className={`${image.id}`}
               onClick={() => {
-                moveCurrenyy(image.id)
-              }}></Dot>
+                moveCurrent(image.id)
+              }}></StyledDot>
           )
         })}
         {/* eslint-disable-next-line @typescript-eslint/no-use-before-define */}
-        <CurrentDot className="" imageIndex={translateValue / 70}></CurrentDot>
-      </DotBox>
-    </Container>
+        <StyledCurrentDot
+          className=""
+          imageIndex={translateValue / 70}></StyledCurrentDot>
+      </StyledDotBox>
+    </StyledCarouselContainer>
   )
 }
 
 export default Carousel
-export const Container = styled.div`
-  position: relative;
-`
-
-const DotBox = styled.div`
-  display: flex;
-  gap: 5px;
-  position: absolute;
-  left: 50%;
-  bottom: -30px;
-  cursor: pointer;
-  transform: translateX(-50%);
-`
-
-const Dot = styled.div`
-  width: 10px;
-  height: 10px;
-  background-color: gray;
-  border-radius: 100px;
-  height: 10px;
-  margin: 0 1px;
-  font-size: 20px;
-`
-
-interface CurrentDotProps {
-  imageIndex: number
-}
-const CurrentDot = styled.div<CurrentDotProps>`
-  width: 10px;
-  height: 10px;
-  background-color: dodgerblue;
-  position: absolute;
-  left: 0;
-  top: 50%;
-  font-size: 20px;
-  margin: 0 1px;
-  border-radius: 100px;
-  cursor: pointer;
-  transform: ${({ imageIndex }) => `translate(${imageIndex * 20}px,-50%)`};
-  transition: transform 0.5s;
-`
--- src/components/carousel/style.ts
+++ src/components/carousel/style.ts
@@ -4,9 +4,9 @@ import styled from '@emotion/styled'
 interface SliderProps {
   cursorOn: boolean
 }
-export const Slider = styled.div<SliderProps>`
+export const StyledSlider = styled.div<SliderProps>`
   position: relative;
-  max-width: 50vw;
+  max-width: 60vw;
   height: 500px;
   display: flex;
   overflow: hidden;
@@ -17,20 +17,20 @@ export const Slider = styled.div<SliderProps>`
 interface ImageBoxProps {
   translateValue: number | null
 }
-export const ImageBox = styled.div<ImageBoxProps>`
+export const StyledImageBox = styled.div<ImageBoxProps>`
   display: flex;
   transition: 1s;
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   transform: ${({ translateValue }) => `translateX(-${translateValue}vw)`};
 `
 
-export const Image = styled.img`
-  width: 50vw;
+export const StyledImage = styled.img`
+  width: 60vw;
   object-fit: cover;
   object-position: center center;
 `
 
-export const ArrowBox = styled.div`
+export const StyledArrowBox = styled.div`
   position: absolute;
   left: -20px;
   top: 0;
@@ -42,11 +42,56 @@ export const ArrowBox = styled.div`
   padding: 20px;
 `
 
-export const Arrow = styled.img`
+export const StyledArrow = styled.img`
+  width: 40px;
+  height: 60px;
+  background-color: #ffffff;
   color: white;
   font-size: 2rem;
   cursor: pointer;
   &:hover {
     color: dodgerblue;
   }
+`
+export const StyledCarouselContainer = styled.div`
+  touch-action: none;
+  position: relative;
+`
+
+export const StyledDotBox = styled.div`
+  display: flex;
+  gap: 5px;
+  position: absolute;
+  left: 50%;
+  bottom: -30px;
+  cursor: pointer;
+  transform: translateX(-50%);
+`
+
+export const StyledDot = styled.div`
+  width: 10px;
+  height: 10px;
+  background-color: #e8e8ea;
+  border-radius: 100px;
+  height: 10px;
+  margin: 0 1px;
+  font-size: 20px;
+`
+
+interface CurrentDotProps {
+  imageIndex: number
+}
+export const StyledCurrentDot = styled.div<CurrentDotProps>`
+  width: 10px;
+  height: 10px;
+  background-color: #2f2e36;
+  position: absolute;
+  left: 0;
+  top: 50%;
+  font-size: 20px;
+  margin: 0 1px;
+  border-radius: 100px;
+  cursor: pointer;
+  transform: ${({ imageIndex }) => `translate(${imageIndex * 20}px,-50%)`};
+  transition: transform 0.5s;
 `

eb52cde3da6786907f046e079a4ecb62601a69c2
sonsurim <surim014@naver.com>
2023-01-04 00:17:21 +0900

    feat: noop 유틸 함수 구현 및 테스트 코드 작성

---
+++ src/utils/common.test.ts
@@ -0,0 +1,11 @@
+import { noop } from './common'
+
+describe('common/noop', () => {
+  it('호출 시, 빈 함수를 반환해야 합니다.', () => {
+    // Given, When
+    const result = noop()
+
+    // Then
+    expect(result).not.toBeDefined()
+  })
+})

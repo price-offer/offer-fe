ee91d77e35c074523ec45b7f0f362ff6afcb5928
shinhyojeong <hyojeonge0@naver.com>
2023-01-08 22:09:06 +0900

    style: import 린트 룰 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import type { AppProps } from 'next/app'
 import { OfferStyleProvider } from '@offer-ui/react'
+import type { AppProps } from 'next/app'
 import type { ReactElement } from 'react'
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => {

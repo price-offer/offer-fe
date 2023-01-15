48384e98e74b8967ff4e3d3af19e2432329f596b
조주영 <jzo09@naver.com>
2023-01-15 20:17:56 +0900

    fix: 테스트 페이지 원상복구

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,12 @@
-import styled from '@emotion/styled'
-import { Avatar } from '@offer-ui/react'
+import { Badge } from '@offer-ui/react'
 import type { NextPage } from 'next'
-import { theme } from '@styles/themes'
+
 const Home: NextPage = () => {
   return (
     <div>
-      <TestAvatar alt="test" src="test" theme={theme}></TestAvatar>
+      <Badge colorType="gray">Badge</Badge>
     </div>
   )
 }
 
 export default Home
-
-interface Test {
-  theme: any
-}
-
-const TestAvatar = styled(Avatar)<Test>`
-  ${({ theme }): string => theme.mediaQuery.tablet} {
-    .img {
-      ${({ theme }): string => theme.avatar.image.large}
-    }
-    ${({ theme }): string => theme.avatar.wrapper.large}
-    background-color: ${({ theme }): string => theme.colors.background.gray90};
-    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.4);
-  }
-`

66e6808f8d2e6697d3119a754ce461b56056b0e8
sonsurim <surim014@naver.com>
2023-01-20 23:10:36 +0900

    feat: useResponsive hook 구현 및 테스트 코드 작성

---
+++ src/hooks/useResponsive.test.ts
@@ -0,0 +1,62 @@
+import type { AvatarProps, ButtonProps } from '@offer-ui/react'
+import { renderHook } from '@testing-library/react'
+import { vi } from 'vitest'
+import { useResponsive } from './useResponsive'
+import { matchMediaQuery } from '@styles'
+
+describe('hooks/useResponsive()', () => {
+  const setViewPort = (device: keyof typeof matchMediaQuery): void => {
+    window.matchMedia = vi.fn().mockImplementation(query => ({
+      addEventListener: vi.fn(),
+      matches: query === matchMediaQuery[device],
+      media: '',
+      onchange: null,
+      removeEventListener: vi.fn()
+    }))
+  }
+
+  it('viewport가 desktop 미디어쿼리 값에 해당하는 경우, AvatarProps의 size 값을 medium으로 리턴해야 합니다.', () => {
+    // Given, When
+    setViewPort('desktop')
+    const { result } = renderHook(() => {
+      return useResponsive<AvatarProps, 'size'>({
+        desktop: 'medium',
+        mobile: 'small',
+        tablet: 'small'
+      })
+    })
+
+    // Then
+    expect(result.current).toBe('medium')
+  })
+
+  it('viewport가 mobile 미디어쿼리 값에 해당하는 경우, AvatarProps의 size 값을 small로 리턴해야 합니다.', () => {
+    // Given, When
+    setViewPort('mobile')
+    const { result } = renderHook(() => {
+      return useResponsive<AvatarProps, 'size'>({
+        desktop: 'medium',
+        mobile: 'small',
+        tablet: 'xsmall'
+      })
+    })
+
+    // Then
+    expect(result.current).toBe('small')
+  })
+
+  it('viewport가 tablet 미디어쿼리 값에 해당하는 경우, ButtonProps의 styleType 값을 outlineDisabled로 리턴해야 합니다.', () => {
+    // Given, When
+    setViewPort('tablet')
+    const { result } = renderHook(() => {
+      return useResponsive<ButtonProps, 'styleType'>({
+        desktop: 'ghost',
+        mobile: 'outline',
+        tablet: 'outlineDisabled'
+      })
+    })
+
+    // Then
+    expect(result.current).toBe('outlineDisabled')
+  })
+})

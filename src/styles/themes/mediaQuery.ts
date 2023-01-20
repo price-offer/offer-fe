4d824c8666dbe2202188f44db5b05d8595c4f747
sonsurim <surim014@naver.com>
2023-01-21 02:04:24 +0900

    feat: 중복타입 제거

--- src/styles/themes/mediaQuery.ts
+++ src/styles/themes/mediaQuery.ts
@@ -11,7 +11,13 @@ export const mediaQuery = {
   tablet: '@media (max-width: 1023px)'
 } as const
 
-export const matchMediaQuery = {
+type MaxWidth = number
+export type Device = keyof typeof mediaQuery
+
+export type MatchMediaQuery = {
+  [key in Device]: `(max-width: ${MaxWidth}px)`
+}
+export const matchMediaQuery: MatchMediaQuery = {
   desktop: '(max-width: 1920px)',
   mobile: '(max-width: 699px)',
   tablet: '(max-width: 1023px)'

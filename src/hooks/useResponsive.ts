e37d66c9396214e8136020f87101cd778837bdfb
sonsurim <surim014@naver.com>
2023-01-21 02:05:10 +0900

    refactor:불필요한 단언 제거, device 옵셔널하게 받을 수 있도록 개선

--- src/hooks/useResponsive.ts
+++ src/hooks/useResponsive.ts
@@ -1,14 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import type { Device, MatchMediaQuery } from '@styles'
 import { matchMediaQuery as defaultMatchMediaQuery } from '@styles'
 
-type Device = 'desktop' | 'tablet' | 'mobile'
-type MaxWidth = number
-
-type ResponsiveCase<T, K extends keyof T> = {
-  [key in Device]: T[K]
-}
-type MatchMediaQuery = {
-  [key in Device]: `(max-width: ${MaxWidth}px)`
+interface ResponsiveCase<T, K extends keyof T> {
+  desktop: T[K]
+  mobile?: T[K]
+  tablet?: T[K]
 }
 type MatchMediaQueryParams = MatchMediaQuery | undefined
 
@@ -18,37 +15,47 @@ type MatchMediaQueryParams = MatchMediaQuery | undefined
  * const avatarSize = useResponsive<AvatarProps, 'size'>({
  *   desktop: 'medium',
  *   tablet: 'small',
- *   mobile: 'small'
+ *   mobile: 'xsmall'
  * });
  */
 export const useResponsive = <T, K extends keyof T>(
   responsiveCase: ResponsiveCase<T, K>,
-  matchMediaQuery: MatchMediaQueryParams = defaultMatchMediaQuery as MatchMediaQuery
+  matchMediaQuery: MatchMediaQueryParams = defaultMatchMediaQuery
 ): T[K] => {
-  const getDevice = (queries: MatchMediaQuery): Device => {
-    const defaultDevice: Device = 'desktop'
+  const getDevice = useCallback(
+    (queries: MatchMediaQuery): Device => {
+      const defaultDevice: Device = 'desktop'
 
-    if (typeof window === 'undefined') {
-      return defaultDevice
-    }
+      if (typeof window === 'undefined') {
+        return defaultDevice
+      }
 
-    return Object.keys(queries).reduce<Device>((prev, cur) => {
-      const device = cur as Device
-      const query = queries[device] as string
+      const device = (Object.keys(queries) as Device[]).reduce(
+        (prev, device) => {
+          const query = queries[device]
 
-      return window.matchMedia(query).matches ? device : prev
-    }, defaultDevice)
-  }
+          return window.matchMedia(query).matches ? device : prev
+        },
+        defaultDevice
+      )
+      const isTablet = device === 'tablet'
+
+      return responsiveCase[device] ? device : isTablet ? 'desktop' : 'tablet'
+    },
+    [responsiveCase]
+  )
 
   const [currentDevice, setCurrentDevice] = useState<Device>(
     getDevice(matchMediaQuery)
   )
 
   useEffect(() => {
-    const matchMedia = Object.keys(matchMediaQuery).map(device => {
-      const query = matchMediaQuery[device as Device] as string
-      return window.matchMedia(query)
-    })
+    const matchMedia = (Object.keys(matchMediaQuery) as Device[]).map(
+      device => {
+        const query = matchMediaQuery[device]
+        return window.matchMedia(query)
+      }
+    )
 
     const handleChange = (): void => {
       setCurrentDevice(getDevice(matchMediaQuery))
@@ -65,7 +72,7 @@ export const useResponsive = <T, K extends keyof T>(
         media.removeEventListener('change', handleChange)
       })
     }
-  }, [matchMediaQuery])
+  }, [matchMediaQuery, getDevice])
 
   return responsiveCase[currentDevice] as T[K]
 }

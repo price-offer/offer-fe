114a3c3a7849f090023a8911b76d598ac85c1c2a
shinhyojeong <hyojeonge0@naver.com>
2023-01-21 16:02:42 +0900

    feat: ProductField 컴포넌트 구현 및 스토리북 적용

---
+++ src/components/product-detail/ProductField/index.tsx
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled'
+import { Divider, Text } from '@offer-ui/react'
+import type { ReactElement } from 'react'
+import type { StyledProps } from '@types'
+
+export interface ProductFieldProps {
+  label: string
+  value: string
+  height?: number
+}
+type StyledTextWrapperProps = StyledProps<ProductFieldProps, 'height'>
+
+export const ProductField = ({
+  label,
+  value,
+  height = 50
+}: ProductFieldProps): ReactElement => {
+  return (
+    <StyledProductField>
+      <Divider direction="vertical" length={`${height}px`} />
+      <StyledTextWrapper height={height}>
+        <Text color="grayScale70" styleType="body02M">
+          {label}
+        </Text>
+        <Text styleType="subtitle01B">{value}</Text>
+      </StyledTextWrapper>
+    </StyledProductField>
+  )
+}
+const StyledProductField = styled.div`
+  display: flex;
+  gap: 12px;
+`
+const StyledTextWrapper = styled.div<StyledTextWrapperProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 4px;
+  height: ${({ height }): string => `${height}px`};
+`

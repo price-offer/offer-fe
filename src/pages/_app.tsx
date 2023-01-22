31f0f4e1cff96c52967b5f3393965609d721518f
조주영 <jzo09@naver.com>
2023-01-22 23:56:37 +0900

    fix: 충돌해결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
-import { OfferStyleProvider, theme as offerTheme } from '@offer-ui/react'
+import { OfferStyleProvider } from '@offer-ui/react'
 import type { AppProps } from 'next/app'
 import type { ReactElement } from 'react'
+import { initMocks } from '../mocks'
+
+initMocks()
 
 const App = ({ Component, pageProps }: AppProps): ReactElement => {
   return (
-    <OfferStyleProvider theme={customTheme}>
+    <OfferStyleProvider>
       <Component {...pageProps} />
     </OfferStyleProvider>
   )

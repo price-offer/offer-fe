8edea609d3c3291690bd8934d4f362e76ad0bfd1
sonsurim <surim014@naver.com>
2023-01-22 21:43:58 +0900

    feat: msw 초기 설정 및 api 구현
    service 관련 타입 정의
    
    refactor: type과 네이밍 동일화

---
+++ src/mocks/fixture.ts
@@ -0,0 +1,94 @@
+import type { MyProfile, MemberProfile, Articles, TradeStatus } from '@types'
+
+export const myProfile: MyProfile = {
+  member: {
+    id: 1,
+    offerLevel: 1,
+    nickname: 'my profile',
+    profileImageUrl: 'https://avatars.githubusercontent.com/u/47546413?v=4',
+    address: '서울시'
+  },
+  sellingArticleCount: 0,
+  likedArticleCount: 0,
+  offerCount: 0,
+  reviewCount: 0
+}
+
+type MemberProfileList = {
+  [key in string]: MemberProfile
+}
+
+export const memberProfileList: MemberProfileList = {
+  1: {
+    member: {
+      id: 1,
+      offerLevel: 1,
+      nickname: 'my profile',
+      profileImageUrl: 'https://avatars.githubusercontent.com/u/47546413?v=4',
+      address: '서울시'
+    },
+    sellingArticleCount: 0,
+    reviewCount: 0
+  },
+  2: {
+    member: {
+      id: 2,
+      offerLevel: 1,
+      nickname: 'other user',
+      profileImageUrl: '',
+      address: '서울시'
+    },
+    sellingArticleCount: 0,
+    reviewCount: 0
+  }
+}
+
+export const updateArticleStatus = (
+  articleId: number,
+  tradeStatus: TradeStatus
+): void => {
+  articles.elements = articles.elements.map(article =>
+    article.id === articleId ? { ...article, tradeStatus } : article
+  )
+}
+
+export const articles: Articles = {
+  elements: [
+    {
+      id: 4,
+      mainImageUrl: 'string',
+      title: 'string',
+      price: 36500,
+      tradeArea: '서울시 강남구',
+      tradeStatus: {
+        code: 4,
+        name: '판매중'
+      },
+      createdDate: '2021-12-10T14:25:30',
+      modifiedDate: '2021-12-10T14:25:30',
+      isLiked: false
+    },
+    {
+      id: 2,
+      mainImageUrl: 'string',
+      title: 'string',
+      price: 8000,
+      tradeArea: '서울시 강남구',
+      tradeStatus: {
+        code: 4,
+        name: '판매중'
+      },
+      createdDate: '2021-12-10T14:23:53',
+      modifiedDate: '2021-12-10T14:23:53',
+      isLiked: false
+    }
+  ],
+  pageInfo: {
+    currentPageNumber: 1,
+    lastPageNumber: 1,
+    sizePerPage: 2,
+    totalElementCount: 2,
+    isFirstPage: true,
+    isLastPage: true
+  }
+}
--- src/setupTest.ts
+++ src/setupTest.ts
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom'
+import { server } from './mocks/server'
+
+beforeAll(() => server.listen())
+afterEach(() => server.restoreHandlers())
+afterAll(() => server.close())

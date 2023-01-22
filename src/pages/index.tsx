d1cf7c0d2616870c59725615eb1d77b2a732ebc3
조주영 <jzo09@naver.com>
2023-01-22 23:51:42 +0900

    fix:index 테스트 파일원상복구

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,12 @@
-import styled from '@emotion/styled'
-import { SelectBox } from '@offer-ui/react'
+import { Badge } from '@offer-ui/react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Mediume
-        colorType="light"
-        items={[
-          {
-            text: '직거래/택배거래',
-            value: '0'
-          },
-          {
-            text: '직거래',
-            value: '1'
-          },
-          {
-            text: '택배거래',
-            value: '2'
-          }
-        ]}
-        placeholder="거래방식"
-        value=""
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        onChange={(): void => {}}></Mediume>
-      <Select
-        colorType="light"
-        items={[
-          {
-            text: '직거래/택배거래',
-            value: '0'
-          },
-          {
-            text: '직거래',
-            value: '1'
-          },
-          {
-            text: '택배거래',
-            value: '2'
-          }
-        ]}
-        placeholder="거래방식"
-        value=""
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        onChange={(): void => {}}></Select>
+      <Badge colorType="gray">Badge</Badge>
     </div>
   )
 }
 
 export default Home
-
-const Select = styled(SelectBox)`
-  ${({ theme }): any => theme.selectBox?.small?.default}
-`
-const Mediume = styled(SelectBox)`
-  ${({ theme }): any => theme.selectBox?.medium?.default}
-`

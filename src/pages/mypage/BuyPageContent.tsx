0475eecd4cca3c32e001ce576d492238e076c5dd
sonsurim <surim014@naver.com>
2023-01-26 00:08:06 +0900

    refactor: mypage tab 컴포넌트 활용하는 형태로 구조 개선

---
+++ src/pages/mypage/BuyPageContent.tsx
@@ -0,0 +1,218 @@
+/* eslint-disable no-console */
+import styled from '@emotion/styled'
+import type { ColorKeys } from '@offer-ui/react'
+import { Divider, SelectBox } from '@offer-ui/react'
+import type { ReactElement, MouseEvent } from 'react'
+import { useState } from 'react'
+import { ProfileBox, Tabs, Tab, BuyTypeArticleList } from '@components'
+import type { TradeBuyActivityType } from '@constants'
+import { TRADE_STATUS, TRADE_ACTIVITY_TYPE } from '@constants'
+import { myProfile } from '@mocks/fixture'
+import type { ArticlesElement } from '@types'
+import { noop } from '@utils'
+
+const sortItems = [
+  {
+    code: 'recently',
+    name: '최신순'
+  }
+]
+
+const tradeStatusList = TRADE_STATUS.filter(item => item.code !== 2)
+const tradeBuyActivityList = Object.entries(TRADE_ACTIVITY_TYPE.buy)
+
+const getArticles = (): ArticlesElement[] => {
+  return Array.from({ length: 10 }, () => 0).map((_, index) => {
+    const tradeStatus =
+      index % 2 !== 0 ? tradeStatusList[0] : tradeStatusList[1]
+
+    return {
+      id: index,
+      mainImageUrl: '',
+      title: `${tradeStatus.name}인 상품`,
+      price: 36500,
+      tradeArea: '서울시 강남구',
+      tradeStatus,
+      createdDate: '2023.01.25',
+      modifiedDate: '2023.01.25',
+      isLiked: false,
+      likeCount: 0,
+      isReviewed: !!(index % 2 !== 0),
+      sellerNickName: 'hypeboy'
+    }
+  })
+}
+
+export const BuyPageContent = (): ReactElement => {
+  const [tabIndex, setTabIndex] = useState<number>(0)
+  const [articles] = useState<ArticlesElement[]>(getArticles())
+
+  const handleTabClick = (
+    e: MouseEvent<HTMLDivElement>,
+    index: number
+  ): void => {
+    setTabIndex(index)
+  }
+
+  return (
+    <div>
+      <StyledContentWrapper>
+        <ProfileBox {...myProfile} />
+        <StyledDivider size="bold" />
+        <StyledUserProductsWrapper>
+          <StyledSearchOptionsWrapper>
+            <Tabs>
+              <StyledTabsList>
+                {tradeBuyActivityList.map((tradeBuyActivity, index) => {
+                  const isCurrent = tabIndex === index
+
+                  return (
+                    <StyledTab
+                      key={tradeBuyActivity[0]}
+                      onClick={handleTabClick}>
+                      <StyledStatusButtonLabel>
+                        <StyledCircle isCurrent={isCurrent} />
+                        <StyledStatusButton isCurrent={isCurrent} type="button">
+                          <StyledText isCurrent={isCurrent}>
+                            {tradeBuyActivity[1]}
+                          </StyledText>
+                        </StyledStatusButton>
+                        <StyledText color="grayScale50">1</StyledText>
+                      </StyledStatusButtonLabel>
+                    </StyledTab>
+                  )
+                })}
+              </StyledTabsList>
+            </Tabs>
+            <SelectBox
+              colorType="none"
+              items={sortItems}
+              value="recently"
+              onChange={noop}
+            />
+          </StyledSearchOptionsWrapper>
+          <StyledProductListWrapper>
+            <BuyTypeArticleList
+              activityType={
+                tradeBuyActivityList[tabIndex][0] as TradeBuyActivityType
+              }
+              articles={articles}
+            />
+          </StyledProductListWrapper>
+        </StyledUserProductsWrapper>
+      </StyledContentWrapper>
+    </div>
+  )
+}
+
+const StyledContentWrapper = styled.div`
+  ${({ theme }): string => `
+    display: flex;
+    gap: 32px;
+
+    ${theme.mediaQuery.tablet} {
+      position: relative;
+      flex-direction: column;
+      gap: 0;
+    }
+  `}
+`
+const StyledSearchOptionsWrapper = styled.div`
+  ${({ theme }): string => `
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+    border-bottom: 1px solid ${theme.colors.grayScale10};
+
+    ${theme.mediaQuery.tablet} {
+      padding: 16px;
+    }
+  `}
+`
+const StyledTabsList = styled(Tabs.List)`
+  display: flex;
+  gap: 22px;
+`
+const StyledTab = styled(Tab)`
+  background: transparent;
+  border: none;
+`
+const StyledStatusButtonLabel = styled.label`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`
+const StyledCircle = styled.span<{ isCurrent: boolean }>`
+  ${({ theme, isCurrent }): string => `
+    content: '';
+    width: 6px;
+    height: 6px;
+    border-radius: 100%;
+    background-color: ${
+      isCurrent ? theme.colors.brandPrimary : theme.colors.grayScale50
+    };
+  `}
+`
+const StyledStatusButton = styled.button<{ isCurrent: boolean }>`
+  ${({ theme }): string => `
+    border: none;
+    background: transparent;
+    padding: 0;
+    margin: 0 4px 0 8px;
+    cursor: pointer;
+
+    ${theme.mediaQuery.tablet} {
+      margin: 0 4px;
+    }
+  `}
+`
+const StyledText = styled.span<{ isCurrent?: boolean; color?: ColorKeys }>`
+  ${({ theme, isCurrent, color }): string => `
+    ${theme.fonts.subtitle01B};
+    color: ${
+      color
+        ? theme.colors[color]
+        : isCurrent
+        ? theme.colors.black
+        : theme.colors.grayScale50
+    };
+
+    ${theme.mediaQuery.tablet} {
+      ${theme.fonts.body02B};
+    }
+  `}
+`
+const StyledUserProductsWrapper = styled.div`
+  ${({ theme }): string => `
+    width: 892px;
+    height: 855px;
+    border: 1px solid ${theme.colors.grayScale10};
+    border-bottom: none;
+
+    ${theme.mediaQuery.tablet} {
+      width: auto;
+      border: none;
+    }
+  `}
+`
+const StyledProductListWrapper = styled.div`
+  ${({ theme }): string => `
+    height: 780px;
+    overflow-y: scroll;
+
+    ${theme.mediaQuery.tablet} {
+      height: auto;
+      overflow-y: unset;
+    }
+  `}
+`
+const StyledDivider = styled(Divider)`
+  ${({ theme }): string => `
+    display: none;
+
+    ${theme.mediaQuery.tablet} {
+      display: block;
+    }
+  `}
+`
---
+++ src/pages/mypage/index.tsx
@@ -0,0 +1,118 @@
+import styled from '@emotion/styled'
+import { Text, Divider } from '@offer-ui/react'
+import type { ReactElement, MouseEvent } from 'react'
+import { useState } from 'react'
+import { BuyPageContent } from './BuyPageContent'
+import { SellPageContent } from './SellPageContent'
+import { Tabs, Tab } from '@components'
+import type { TradeActivityType, TradeActivityName } from '@constants'
+
+interface PageTab {
+  code: TradeActivityType
+  name: TradeActivityName
+}
+const pageTabs: PageTab[] = [
+  {
+    code: 'sell',
+    name: '판매'
+  },
+  {
+    code: 'buy',
+    name: '구매'
+  },
+  {
+    code: 'review',
+    name: '후기'
+  }
+]
+
+const MyPage = (): ReactElement => {
+  const [pageIndex, setPageIndex] = useState<number>(0)
+
+  const handleTabClick = (
+    e: MouseEvent<HTMLDivElement>,
+    index: number
+  ): void => {
+    setPageIndex(index)
+  }
+
+  return (
+    <div>
+      <StyledUserName styleType="headline02B">
+        닉네임님의 거래 활동
+      </StyledUserName>
+      <Divider />
+
+      <Tabs>
+        <StyledLayout>
+          <Tabs.List>
+            {pageTabs.map((_page, index) => (
+              <StyledTab
+                key={_page.code}
+                isSelected={pageIndex === index}
+                onClick={handleTabClick}>
+                {_page.name}
+              </StyledTab>
+            ))}
+          </Tabs.List>
+        </StyledLayout>
+        <Divider />
+        <StyledLayout>
+          <StyledTabPanels>
+            <Tabs.Panel>
+              <SellPageContent />
+            </Tabs.Panel>
+            <Tabs.Panel>
+              <BuyPageContent />
+            </Tabs.Panel>
+            <Tabs.Panel>후기</Tabs.Panel>
+          </StyledTabPanels>
+        </StyledLayout>
+      </Tabs>
+      <Divider />
+    </div>
+  )
+}
+
+export default MyPage
+
+const StyledUserName = styled(Text)`
+  ${({ theme }): string => `
+    display: block;
+    max-width: 1200px;
+    margin: 20px auto;
+
+    ${theme.mediaQuery.tablet} {
+      ${theme.fonts.body01B};
+      margin: 16px auto;
+    }
+  `}
+`
+const StyledLayout = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`
+const StyledTab = styled(Tab)<{ isSelected: boolean }>`
+  ${({ theme, isSelected }): string => `
+    padding: 17px 16px;
+    color: ${isSelected ? theme.colors.white : theme.colors.black};
+    background-color: ${isSelected ? theme.colors.black : theme.colors.white};
+    border: none;
+    cursor: pointer;
+    ${theme.fonts.subtitle01B};
+
+    ${theme.mediaQuery.tablet} {
+      ${theme.fonts.body01B};
+      padding: 16px 24px;
+    }
+  `}
+`
+const StyledTabPanels = styled(Tabs.Panels)`
+  ${({ theme }): string => `
+    padding-top: 40px;
+
+    ${theme.mediaQuery.tablet} {
+      padding-top: 0;
+    }
+  `}
+`

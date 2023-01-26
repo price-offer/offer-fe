f1e78bf4623df7224fb85a0f5f83110d6a4f58d2
sonsurim <surim014@naver.com>
2023-01-26 23:52:44 +0900

    feat: reviews fixture 정의

--- src/mocks/fixture.ts
+++ src/mocks/fixture.ts
@@ -1,4 +1,4 @@
-import type { MyProfile, MemberProfile, Articles } from '@types'
+import type { MyProfile, MemberProfile, Articles, Reviews } from '@types'
 
 export const myProfile: MyProfile = {
   member: {
@@ -85,6 +85,70 @@ export const articles: Articles = {
     lastPageNumber: 1,
     sizePerPage: 2,
     totalElementCount: 2,
+    isFirstPage: true,
+    isLastPage: true
+  }
+}
+
+export const reviews: Reviews = {
+  elements: [
+    {
+      id: 4,
+      reviewer: {
+        id: 1,
+        profileImageUrl: 'http://~~~',
+        nickname: 'awesomeo184',
+        offerLevel: 1
+      },
+      article: {
+        id: 1,
+        title: 'exam title'
+      },
+      score: 2,
+      content: '쿨거래 조아요',
+      isWritingAvailableFromCurrentMember: true,
+      createdDate: '2021-12-02T19:19:18'
+    },
+    {
+      id: 5,
+      reviewer: {
+        id: 1,
+        profileImageUrl: 'http://~~~',
+        nickname: 'awesomeo184',
+        offerLevel: 1
+      },
+      article: {
+        id: 1,
+        title: 'exam title'
+      },
+      score: 2,
+      content: '쿨거래 조아요',
+      isWritingAvailableFromCurrentMember: false,
+      createdDate: '2021-12-02T19:19:18'
+    },
+    {
+      id: 6,
+      reviewer: {
+        id: 1,
+        profileImageUrl: 'http://~~~',
+        nickname: 'awesomeo184',
+        offerLevel: 1
+      },
+      article: {
+        id: 1,
+        title: 'exam title'
+      },
+      score: 2,
+      content: '쿨거래 조아요',
+      isWritingAvailableFromCurrentMember: true,
+      createdDate: '2021-12-02T19:19:18'
+    }
+  ],
+  pageInfo: {
+    currentPageNumber: 1,
+    lastPageNumber: 1,
+    sizePerPage: 10,
+    totalElementCount: 3,
     isFirstPage: true,
     isLastPage: true
   }

39b1ac747128c771adddac0081a11112645bc42c
shinhyojeong <hyojeonge0@naver.com>
2023-02-02 22:48:16 +0900

    feat: PostingForm 최대 높이에 따라서 정렬 변경 구현

--- src/components/posting/PostingForm/index.tsx
+++ src/components/posting/PostingForm/index.tsx
@@ -1,29 +1,72 @@
 import styled from '@emotion/styled'
 import { Text } from '@offer-ui/react'
 import type { ReactElement, ReactNode } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 export interface PostingFormProps {
   label: string
   children: ReactNode
 }
 
+const MAX_CONTENT_HEIGHT = 48
+interface StyledPostingFormProps {
+  formAlign: FormAlign
+}
+type FormAlign = 'start' | 'center'
+
 export const PostingForm = ({
   label,
   children
 }: PostingFormProps): ReactElement => {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+  const [formAlign, setFormAlign] = useState<FormAlign>('center')
+
+  useEffect(() => {
+    const handleFormAlign = (): void => {
+      if (wrapperRef.current) {
+        const { clientHeight: wrapperClientHeight } = wrapperRef.current
+        const alignType =
+          MAX_CONTENT_HEIGHT < Number(wrapperClientHeight) ? 'start' : 'center'
+        setFormAlign(alignType)
+      }
+    }
+
+    window.addEventListener('resize', handleFormAlign)
+
+    return () => {
+      window.removeEventListener('resize', handleFormAlign)
+    }
+  }, [])
+
   return (
-    <StyledPostingForm>
+    <StyledPostingForm ref={wrapperRef} formAlign={formAlign}>
       <StyledLabel styleType="body01B">{label}</StyledLabel>
       {children}
     </StyledPostingForm>
   )
 }
 
-const StyledPostingForm = styled.div`
+const StyledPostingForm = styled.div<StyledPostingFormProps>`
   display: flex;
   gap: 20px;
-  align-items: center;
+  align-items: ${({ formAlign }): string => formAlign};
 `
 const StyledLabel = styled(Text)`
   width: 100px;
+
+  ${({ theme }): string => `
+    ${theme.mediaQuery.tablet} {
+      min-width: 60px;
+      width: 60px;
+
+      ${theme.fonts.body02B}
+    }
+
+    ${theme.mediaQuery.mobile} {
+      min-width: 60px;
+      width: 60px;
+      
+      ${theme.fonts.body02B}
+    }
+  `}
 `

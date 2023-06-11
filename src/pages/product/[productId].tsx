83cce259140a0d0d6da527689a25659d089db313
shinhyojeong <hyojeonge0@naver.com>
2023-06-11 23:11:49 +0900

    design: 스타일드 컴포넌트 네이밍 변경, 스타일 수정

--- src/pages/product/[productId].tsx
+++ src/pages/product/[productId].tsx
@@ -1,193 +1,196 @@
+import { css } from '@emotion/react'
+import type { SerializedStyles } from '@emotion/react'
 import styled from '@emotion/styled'
-import { Carousel, SelectBox, Divider, Text, IconButton } from '@offer-ui/react'
+import { Carousel, Divider, Text, IconButton, SelectBox } from '@offer-ui/react'
 import type { ReactElement } from 'react'
 import { PriceOfferCard } from '@components/product-detail/PriceOfferCard'
 import type { Offer } from '@components/product-detail/PriceOfferCard'
 import { ProductField } from '@components/product-detail/ProductField'
 import { UserProfile } from '@components/product-detail/UserProfile'
 
-const ProductDetailPage = (): ReactElement => {
-  const images = [
-    {
-      id: 1,
-      url: 'https://picsum.photos/200/300'
-    },
-    {
-      id: 2,
-      url: 'https://picsum.photos/200/300'
-    },
-    {
-      id: 3,
-      url: 'https://picsum.photos/200/300'
-    },
-    {
-      id: 4,
-      url: 'https://picsum.photos/200/300'
-    },
-    {
-      id: 5,
-      url: 'https://picsum.photos/200/300'
-    }
-  ]
-  const offerList = [
-    {
-      id: 23,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 22343,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 2223433,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 224323,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 215913,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 217813,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 210913,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 215613,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 211473,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 2187913,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 214513,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 215613,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 277113,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    },
-    {
-      id: 21138,
-      name: '신효정',
-      level: 23,
-      tradeArea: '시당구 동작동',
-      date: '1시간 전',
-      offerPrice: 234233,
-      profileUrl: '',
-      tradeMethod: 'all'
-    }
-  ]
-  const categories = [
-    { label: '작성일', value: '1시간 전' },
-    { label: '상품 상태', value: '새상품' },
-    { label: '거래 방식', value: '직거래' },
-    { label: '거래 지역', value: '동작구 사당동' }
-  ]
+const IMAGES_MOCK = [
+  {
+    id: 1,
+    url: 'https://picsum.photos/200/300'
+  },
+  {
+    id: 2,
+    url: 'https://picsum.photos/200/300'
+  },
+  {
+    id: 3,
+    url: 'https://picsum.photos/200/300'
+  },
+  {
+    id: 4,
+    url: 'https://picsum.photos/200/300'
+  },
+  {
+    id: 5,
+    url: 'https://picsum.photos/200/300'
+  }
+]
 
+const OFFERS_MOCK: Offer[] = [
+  {
+    id: 23,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 22343,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 2223433,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 224323,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 215913,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 217813,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 210913,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 215613,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 211473,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 2187913,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 214513,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 215613,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 277113,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  },
+  {
+    id: 21138,
+    name: '신효정',
+    level: 23,
+    tradeArea: '시당구 동작동',
+    date: '1시간 전',
+    offerPrice: 234233,
+    profileUrl: '',
+    tradeMethod: 'all'
+  }
+]
+const CATEGORIES_MOCK = [
+  { label: '작성일', value: '1시간 전' },
+  { label: '상품 상태', value: '새상품' },
+  { label: '거래 방식', value: '직거래' },
+  { label: '거래 지역', value: '동작구 사당동' }
+]
+
+const ProductDetailPage = (): ReactElement => {
   return (
-    <StyledProductDetailPage>
-      <StyledMainContainer>
+    <Layout>
+      <Main>
         <div>
-          <Carousel images={images} isArrow name="product" />
+          <Carousel images={IMAGES_MOCK} isArrow name="product" />
         </div>
-        <StyledProductInfo>
+        <div>
           <div>
-            <StyledProductStatus>
-              <StyledSelectBox
+            <ProductStatus>
+              <ProductStatusSelectBox
                 items={[
                   { code: 1, name: '판매중' },
                   { code: 2, name: '거래 완료' }
@@ -198,26 +201,26 @@ const ProductDetailPage = (): ReactElement => {
                 }}
               />
               <IconButton icon="more" size={24} />
-            </StyledProductStatus>
+            </ProductStatus>
             <Text color="grayScale70" styleType="body02M" tag="p">
               카테고리
             </Text>
-            <StyledProductName styleType="headline01B" tag="p">
+            <ProductName styleType="headline01B" tag="p">
               제품명
-            </StyledProductName>
+            </ProductName>
             <Text styleType="display01B" tag="p">
               50000
               <Text styleType="subtitle01M">원</Text>
             </Text>
           </div>
-          <StyledProfileDivider gap={16} />
+          <ProfileDivider gap={16} />
           <div>
             <Text styleType="headline02B">상품 정보</Text>
-            <StyledTransactionWrapper>
-              {categories.map(({ label, value }) => (
+            <TransactionContainer>
+              {CATEGORIES_MOCK.map(({ label, value }) => (
                 <ProductField key={label} label={label} value={value} />
               ))}
-            </StyledTransactionWrapper>
+            </TransactionContainer>
             <Text styleType="body02R" tag="p">
               인텔리전스랩스는 다양한 게임 정보를 활용해 ‘빅데이터’,
               ‘머신러닝·딥러닝’, ‘인공지능(AI)’ 기술과 공학적 사고를 통해
@@ -226,7 +229,7 @@ const ProductDetailPage = (): ReactElement => {
               소속되어 유저 인사이트 기반으로 서비스가 성장하도록 돕습니다.
             </Text>
           </div>
-          <StyledProfileDivider gap={16} />
+          <ProfileDivider gap={16} />
           <UserProfile
             image="https://picsum.photos/200/300"
             level={1}
@@ -234,20 +237,16 @@ const ProductDetailPage = (): ReactElement => {
             nickName="신효정"
             type="basic"
           />
-        </StyledProductInfo>
-      </StyledMainContainer>
-      <StyledCardDivider size="bold" />
-      <PriceOfferCard
-        isLike={false}
-        likeCount={3}
-        offerList={offerList as Offer[]}
-      />
-    </StyledProductDetailPage>
+        </div>
+      </Main>
+      <MainDivider size="bold" />
+      <PriceOfferCard isLike={false} likeCount={3} offerList={OFFERS_MOCK} />
+    </Layout>
   )
 }
 
-const StyledProfileDivider = styled(Divider)`
-  ${({ theme }): string => `
+const ProfileDivider = styled(Divider)`
+  ${({ theme }): SerializedStyles => css`
     ${theme.mediaQuery.tablet} {
       margin: 20px 0;
     }
@@ -256,34 +255,38 @@ const StyledProfileDivider = styled(Divider)`
     }
   `}
 `
-const StyledProductStatus = styled.div`
+const ProductStatus = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
 `
-const StyledProductDetailPage = styled.div`
+const Layout = styled.div`
   display: flex;
   gap: 35px;
-  margin: 25px 0 15px;
-  width: 100vw;
+  margin: 25px auto 15px;
+  max-width: 1200px;
+  width: 100%;
 
-  ${({ theme }): string => `
+  ${({ theme }): SerializedStyles => css`
     ${theme.mediaQuery.tablet} {
       flex-direction: column;
       gap: 0;
       margin: 0;
+      padding: 0 24px;
     }
+
     ${theme.mediaQuery.mobile} {
       flex-direction: column;
       gap: 0;
       margin: 0;
+      padding: 0 16px;
     }
   `}
 `
-const StyledCardDivider = styled(Divider)`
+const MainDivider = styled(Divider)`
   display: none;
 
-  ${({ theme }): string => `
+  ${({ theme }): SerializedStyles => css`
     ${theme.mediaQuery.tablet} {
       display: block;
       margin: 22px 0 16px;
@@ -294,8 +297,8 @@ const StyledCardDivider = styled(Divider)`
     }
   `}
 `
-const StyledMainContainer = styled.div`
-  ${({ theme }): string => `
+const Main = styled.div`
+  ${({ theme }): SerializedStyles => css`
     ${theme.mediaQuery.tablet} {
       margin: 20px 24px 0;
     }
@@ -305,10 +308,10 @@ const StyledMainContainer = styled.div`
   `}
 `
 
-const StyledSelectBox = styled(SelectBox)`
+const ProductStatusSelectBox = styled(SelectBox)`
   margin: 33px 0 16px;
 
-  ${({ theme }): string => `
+  ${({ theme }): SerializedStyles => css`
     ${theme.mediaQuery.tablet} {
       margin: 20px 0;
     }
@@ -317,13 +320,12 @@ const StyledSelectBox = styled(SelectBox)`
     }
   `}
 `
-const StyledProductInfo = styled.div``
-const StyledTransactionWrapper = styled.div`
+const TransactionContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 24px 0;
 
-  ${({ theme }): string => `
+  ${({ theme }): SerializedStyles => css`
     ${theme.mediaQuery.tablet} {
       display: grid;
       grid-template-columns: 152px 152px;
@@ -342,10 +344,10 @@ const StyledTransactionWrapper = styled.div`
     }
   `}
 `
-const StyledProductName = styled(Text)`
+const ProductName = styled(Text)`
   margin: 4px 0 8px;
 
-  ${({ theme }): string => `
+  ${({ theme }): SerializedStyles => css`
     ${theme.mediaQuery.tablet} {
       margin: 2px 0 8px;
     }

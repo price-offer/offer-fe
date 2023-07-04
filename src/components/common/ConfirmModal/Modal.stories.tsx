0865ad9ab829f8fdb246054d7fc49ae83aa62f32
조주영 <jooyoung@finda.co.kr>
2023-07-04 10:54:54 +0900

    모달 원복

--- src/components/common/ConfirmModal/Modal.stories.tsx
+++ src/components/common/ConfirmModal/Modal.stories.tsx
@@ -1,43 +1,41 @@
-export {}
+import type { Meta, Story } from '@storybook/react'
+import { useState } from 'react'
+import { ConfirmModal } from './index'
+import type { ConfirmModalProps } from './index'
 
-// import type { Meta, Story } from '@storybook/react'
-// import { useState } from 'react'
-// import { ConfirmModal } from './index'
-// import type { ConfirmModalProps } from './index'
+export default {
+  component: ConfirmModal,
+  title: 'Components/Common/ConfirmModal'
+} as Meta<ConfirmModalProps>
 
-// export default {
-//   component: ConfirmModal,
-//   title: 'Components/Common/ConfirmModal'
-// } as Meta<ConfirmModalProps>
+const Template: Story<ConfirmModalProps> = args => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
-// const Template: Story<ConfirmModalProps> = args => {
-//   const [isOpen, setIsOpen] = useState<boolean>(false)
+  return (
+    <>
+      <button
+        type="button"
+        onClick={(): void => {
+          setIsOpen(true)
+        }}>
+        click
+      </button>
+      <ConfirmModal
+        {...args}
+        isOpen={isOpen}
+        onClose={(): void => {
+          setIsOpen(false)
+        }}
+      />
+    </>
+  )
+}
 
-//   return (
-//     <>
-//       <button
-//         type="button"
-//         onClick={(): void => {
-//           setIsOpen(true)
-//         }}>
-//         click
-//       </button>
-//       <ConfirmModal
-//         {...args}
-//         isOpen={isOpen}
-//         onClose={(): void => {
-//           setIsOpen(false)
-//         }}
-//       />
-//     </>
-//   )
-// }
-
-// export const Default = Template.bind({})
-// Default.args = {
-//   description: '지금 바로 Offer에서 첫 거래를 시작해볼까요?',
-//   ghostButtonText: '괜찮아요',
-//   hasLogo: true,
-//   primaryButtonText: '첫 거래하기',m
-//   title: '부드러운 냉장고님 반가워요!'
-// }
+export const Default = Template.bind({})
+Default.args = {
+  description: '지금 바로 Offer에서 첫 거래를 시작해볼까요?',
+  ghostButtonText: '괜찮아요',
+  hasLogo: true,
+  primaryButtonText: '첫 거래하기',
+  title: '부드러운 냉장고님 반가워요!'
+}

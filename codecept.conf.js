71677242e3e4a41de0f2432ff7ef4d21e6210599
조주영 <jooyoung@finda.co.kr>
2023-07-15 18:18:13 +0900

    chore:테스팅 셋팅

---
+++ codecept.conf.js
@@ -0,0 +1,24 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      browser: 'chromium',
+      url: 'http://localhost:8080',
+      show: true
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'offer-fe'
+}

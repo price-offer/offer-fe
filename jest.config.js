61572e5fc72408448f3afca39bfe072ef78bcce1
조주영 <jooyoung@finda.co.kr>
2023-07-30 22:59:28 +0900

    코드리뷰 반영+ vitest.config 제거

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,12 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 module.exports = {
-  // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
 
-  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: false,
 
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
 
-  // An array of file extensions your modules use
   moduleFileExtensions: [
     'js',
     'mjs',
@@ -28,10 +18,9 @@ module.exports = {
     'node'
   ],
 
-  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^@apis(.*)$': '<rootDir>/src/apis/$1', // 수정된 줄
+    '^@apis(.*)$': '<rootDir>/src/apis/$1',
     '^@mocks(.*)$': '<rootDir>/src/mocks/$1',
     '^@assets(.*)$': '<rootDir>/src/assets/$1',
     '^@components(.*)$': '<rootDir>/src/components/$1',
@@ -47,19 +36,13 @@ module.exports = {
   },
 
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // jest<=>바벨설정
-    '^.+\\.svg$': 'jest-transformer-svg' // svg 인식 설정
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    '^.+\\.svg$': 'jest-transformer-svg'
   },
 
-  // The test environment that will be used for testing
   testEnvironment: 'jest-environment-node',
 
-  // The glob patterns Jest uses to detect test files
-  testMatch: [
-    '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
-    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
-  ],
+  testMatch: ['<rootDir>/**/*.test.(js|jsx|ts|tsx)'],
 
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ['<rootDir>/node_modules/']
 }

fbbea67376ed5c7c75d1382d7c6c3f41abee086c
shinhyojeong <hyojeonge0@naver.com>
2023-08-19 18:12:35 +0900

    feat: 모달 사용시 필요한 액션, prop 추가

--- src/components/product/PriceOfferModal/PriceOfferModal.stories.tsx
+++ src/components/product/PriceOfferModal/PriceOfferModal.stories.tsx
@@ -1,12 +1,15 @@
 import type { Meta, Story } from '@storybook/react'
+import type { PriceOfferModalProps } from './types'
 import { PriceOfferModal } from './index'
 
 export default {
   component: PriceOfferModal,
   title: 'Product/PriceOfferModal'
-} as Meta
+} as Meta<PriceOfferModalProps>
 
-const Template: Story = args => <PriceOfferModal {...args} />
+const Template: Story<PriceOfferModalProps> = args => (
+  <PriceOfferModal {...args} />
+)
 
 export const Default = Template.bind({})
 Default.args = {}

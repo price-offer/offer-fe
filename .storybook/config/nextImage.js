6665210996a452e8c3fb11134d71752dc5d46a21
sonsurim <surim014@naver.com>
2023-08-22 20:34:05 +0900

    config: next/image 설정 파일 작업

---
+++ .storybook/config/nextImage.js
@@ -0,0 +1,9 @@
+import * as NextImage from 'next/image'
+
+const OriginalNextImage = NextImage.default
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: props => <OriginalNextImage {...props} unoptimized />
+})
+
+export default OriginalNextImage

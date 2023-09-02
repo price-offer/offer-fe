98caa76021946f8abf59ee3a9085583dbbc9755a
조주영 <jooyoung@finda.co.kr>
2023-09-02 14:11:33 +0900

    코드리뷰반영

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { OfferStyleProvider, theme as offerTheme } from '@offer-ui/react'
 import type { AppProps } from 'next/app'
 import type { ReactElement } from 'react'
-import { useState } from 'react'
 import { theme } from '@styles'
 
 const customTheme = {
@@ -9,24 +8,13 @@ const customTheme = {
   ...theme
 }
 
+const isUseMock = process.env.NEXT_PUBLIC_API_MOCKING === 'enabled'
+
+if (isUseMock) {
+  await import('../mocks')
+}
+
 const App = ({ Component, pageProps }: AppProps): ReactElement | null => {
-  const isUseMock = process.env.NEXT_PUBLIC_API_MOCKING === 'enabled'
-
-  const [isReadyMock, setIsReadyMock] = useState<boolean>(!isUseMock)
-
-  if (isUseMock) {
-    const mock = async (): Promise<any> => {
-      const { initMocks } = await import('../mocks')
-      await initMocks()
-      setIsReadyMock(true)
-    }
-    mock()
-  }
-
-  if (!isReadyMock) {
-    return null
-  }
-
   return (
     <OfferStyleProvider theme={customTheme}>
       <Component {...pageProps} />

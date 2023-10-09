b8f297f74c6c3b7b2b08ff6eca726397214da76e
shinhyojeong <hyojeonge0@naver.com>
2023-10-09 23:55:57 +0900

    feat: 날짜 formatting 패키지 설치 및 사용을 위한 util 구현

--- src/utils/format/index.ts
+++ src/utils/format/index.ts
@@ -1,3 +1,18 @@
+import dayjs from 'dayjs'
+import 'dayjs/locale/ko'
+
+dayjs.locale('ko')
+
+const DATE_FORMAT = {
+  'YYYY년 M월 D일 dddd요일': 'YYYY년 M월 D일 dddd요일',
+  'A H:m': 'A H:m'
+}
+
+export const formatDate = (
+  date: string | Date,
+  format: keyof typeof DATE_FORMAT
+) => dayjs(date).format(format)
+
 export const toLocaleCurrency = (value: number): string => {
   return value.toLocaleString('kr')
 }

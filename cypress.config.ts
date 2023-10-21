a406d6eb5db3d74a520e20e1573fd22bac018592
chojooyoung <jzo09@naver.com>
2023-10-21 23:10:36 +0900

    fix: chrome add

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'cypress'
 
+import { defineConfig } from 'cypress'
+
 export default defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      return {
+        browser: {
+          name: 'chrome',
+          family: 'chromium',
+          channel: 'stable',
+          displayName: 'Chrome',
+          versionRegex: /Google Chrome (\S+)/m,
+          binary: ['google-chrome', 'chrome', 'google-chrome-stable']
+        }
+      }
+    },
     specPattern: 'src/tests/e2e/**/*.cy.ts',
     supportFile: 'src/tests/e2e/support/commands.ts',
     fixturesFolder: 'src/tests/e2e/'

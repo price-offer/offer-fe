c38e0ececc4a5c52ea13de23d503cfbbca01e597
shinhyojeong <hyojeonge0@naver.com>
2023-11-14 21:11:43 +0900

    feat: jotai 설정 복구

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  presets: ['next/babel', '@babel/preset-env', '@babel/preset-typescript']
+  presets: [
+    'next/babel',
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    ['@babel/preset-react', { runtime: 'automatic' }]
+  ],
+  plugins: ['jotai/babel/plugin-react-refresh']
 }

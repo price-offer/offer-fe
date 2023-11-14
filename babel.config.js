4b40ab82566be4f9e5ace38dab8f2c47ca8a105e
shinhyojeong <hyojeonge0@naver.com>
2023-11-14 20:54:29 +0900

    chore: jotai 패키지 및 설정 제거

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  presets: [
-    'next/babel',
-    '@babel/preset-env',
-    '@babel/preset-typescript',
-    ['@babel/preset-react', { runtime: 'automatic' }]
-  ],
-  plugins: ['jotai/babel/plugin-react-refresh']
+  presets: ['next/babel', '@babel/preset-env', '@babel/preset-typescript']
 }
--- src/apis/sample/api.ts
+++
@@ -1,7 +0,0 @@
-import type { AxiosResponse } from 'axios'
-import axios from 'axios'
-import type { SampleReq, SampleRes } from './types'
-
-export const getSample = async (
-  param: SampleReq
-): Promise<AxiosResponse<SampleRes>> => axios.get(`/sample?${param}`)

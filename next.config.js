90476bebca8c52f8cd8fc5e8c34e2567f06be1fb
shinhyojeong <hyojeonge0@naver.com>
2023-11-15 16:06:19 +0900

    feat: strict 모드 off

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { default: DevServer } = require('next/dist/server/dev/next-dev-server')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
-  reactStrictMode: true
+  reactStrictMode: false
 }
 
 module.exports = nextConfig

da8640af11d245f2a4de6f993aaea35c4dbf89ad
shinhyojeong <hyojeonge0@naver.com>
2023-11-15 15:54:41 +0900

    feat: querystring으로 변환하는 유틸 구현 및 테스트

--- src/constants/oauth.ts
+++ src/constants/oauth.ts
@@ -1,7 +1,13 @@
+import { toQueryString } from '@utils/format'
+
 const OAUTH_ENDPOINT = {
-  KAKAO: 'http://kauth.kakao.com/oauth/authorize'
+  KAKAO: 'https://kauth.kakao.com/oauth/authorize'
 }
 
 export const OAUTH_URL = {
-  KAKAO: `${OAUTH_ENDPOINT.KAKAO}?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`
+  KAKAO: `${OAUTH_ENDPOINT.KAKAO}${toQueryString({
+    response_type: 'code',
+    client_id: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY || '',
+    redirect_uri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI || ''
+  })}`
 }

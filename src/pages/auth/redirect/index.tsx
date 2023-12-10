d33e126649c77debbc53f0dc94609f5352b0aab9
shinhyojeong <hyojeonge0@naver.com>
2023-12-11 00:38:21 +0900

    feat: middleware에서 로그인 후 redirect 하도록

--- src/pages/auth/redirect/index.tsx
+++ src/pages/auth/redirect/index.tsx
@@ -1,37 +1,5 @@
-import { useSearchParams } from 'next/navigation'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { useLoginQuery } from '@apis'
-
 const AuthRedirectionPage = () => {
-  const router = useRouter()
-  const searchParams = useSearchParams()
-  const [kakaoCode, setKakaoCode] = useState('')
-  const { data: userData } = useLoginQuery(kakaoCode)
-
-  const getKakaoCode = async () => {
-    const code = searchParams.get('code')
-
-    if (code) {
-      setKakaoCode(code)
-    }
-  }
-
-  const updateUserAtom = async () => {
-    if (userData) {
-      router.replace('/')
-    }
-  }
-
-  useEffect(() => {
-    getKakaoCode()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchParams])
-
-  useEffect(() => {
-    updateUserAtom()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userData])
+  return <div>loading...</div>
 }
 
 export default AuthRedirectionPage

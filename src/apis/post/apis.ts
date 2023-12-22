424ddc37ba79367531b759016ed06f2e2f3ea0cc
shinhyojeong <hyojeonge0@naver.com>
2023-12-22 20:47:32 +0900

    fix: 타입 에러 수정

--- src/apis/post/apis.ts
+++ src/apis/post/apis.ts
@@ -1,5 +1,8 @@
-import type { GetCategoriesRes } from './types'
+import type { GetCategoriesRes, GetPostsReq, GetPostsRes } from './types'
 import { http } from '@utils/http'
 
 export const getCategories = () =>
   http.get<null, GetCategoriesRes>('/categories')
+
+export const getPosts = (params: GetPostsReq) =>
+  http.get<GetPostsReq, GetPostsRes>('/posts', params)
--- src/apis/post/queries.ts
+++ src/apis/post/queries.ts
@@ -1,8 +1,15 @@
 import { useQuery } from '@tanstack/react-query'
-import { getCategories } from './apis'
+import { getCategories, getPosts } from './apis'
+import type { GetPostsReq } from './types'
 
 export const useGetCategoriesQuery = () =>
   useQuery({
     queryKey: ['getCategories'],
     queryFn: getCategories
+  })
+
+export const useGetPostsQuery = (params: GetPostsReq) =>
+  useQuery({
+    queryKey: ['getPosts'],
+    queryFn: () => getPosts(params)
   })

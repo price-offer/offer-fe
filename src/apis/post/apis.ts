42ce732d1828c5a4e600f638f506db96d31efd65
sonsurim <surim014@naver.com>
2023-12-26 02:08:16 +0900

    add updatePostTradeStatus api and query

--- src/apis/post/apis.ts
+++ src/apis/post/apis.ts
@@ -1,4 +1,10 @@
-import type { GetCategoriesRes, GetPostsReq, GetPostsRes } from './types'
+import type {
+  GetCategoriesRes,
+  GetPostsReq,
+  GetPostsRes,
+  UpdateTradeStatusReq,
+  UpdateTradeStatusRes
+} from './types'
 import { http } from '@utils/http'
 
 export const getCategories = () =>
@@ -6,3 +12,9 @@ export const getCategories = () =>
 
 export const getPosts = (params: GetPostsReq) =>
   http.get<GetPostsReq, GetPostsRes>('/posts', params)
+
+export const updatePostTradeStatus = (params: UpdateTradeStatusReq) =>
+  http.put<UpdateTradeStatusReq, UpdateTradeStatusRes>(
+    `/posts/trade-status/${params.postId}`,
+    params
+  )
--- src/apis/post/queries.ts
+++ src/apis/post/queries.ts
@@ -1,6 +1,12 @@
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
-import { getCategories, getPosts } from './apis'
-import type { GetPostsReq, GetPostsRes } from './types'
+import type { DefaultError } from '@tanstack/react-query'
+import { useInfiniteQuery, useMutation, useQuery } from '@tanstack/react-query'
+import { getCategories, getPosts, updatePostTradeStatus } from './apis'
+import type {
+  GetPostsReq,
+  GetPostsRes,
+  UpdateTradeStatusReq,
+  UpdateTradeStatusRes
+} from './types'
 
 export const useGetCategoriesQuery = () =>
   useQuery({
@@ -8,19 +14,24 @@ export const useGetCategoriesQuery = () =>
     queryFn: getCategories
   })
 
-export const useGetPostsQuery = (params: GetPostsReq) =>
+export const useGetPostsQuery = (searchOptions: GetPostsReq) =>
   useQuery({
-    queryKey: ['getPosts'],
-    queryFn: () => getPosts(params)
+    queryKey: ['posts', searchOptions],
+    queryFn: () => getPosts(searchOptions)
   })
 
 export const useGetInfinitePostsQuery = (params: GetPostsReq) =>
   useInfiniteQuery<GetPostsRes>({
-    queryKey: ['getPosts'],
+    queryKey: ['infinitePosts'],
     queryFn: () => getPosts(params),
     initialPageParam: null,
     getNextPageParam: lastPage =>
       lastPage?.hasNext
         ? lastPage.posts[lastPage.posts.length - 1].id
         : undefined
+  })
+
+export const usePostTradeStatusMutation = () =>
+  useMutation<UpdateTradeStatusRes, DefaultError, UpdateTradeStatusReq>({
+    mutationFn: params => updatePostTradeStatus(params)
   })

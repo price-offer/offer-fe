448e3a6c42d049bf5306b32b70204378178c0e37
shinhyojeong <hyojeonge0@naver.com>
2024-01-03 17:16:54 +0900

    feat: 게시글 삭제 api 연결

--- src/pages/post/[postId]/index.tsx
+++ src/pages/post/[postId]/index.tsx
@@ -1,15 +1,33 @@
 import { css } from '@emotion/react'
 import type { SerializedStyles } from '@emotion/react'
 import styled from '@emotion/styled'
-import { Carousel, Divider, Text, IconButton, SelectBox } from '@offer-ui/react'
+import {
+  Carousel,
+  Divider,
+  Text,
+  IconButton,
+  SelectBox,
+  Button
+} from '@offer-ui/react'
 import type { GetServerSideProps } from 'next'
 import Link from 'next/link'
-import { useState, type ReactElement, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useState, type ReactElement } from 'react'
 import { getTimeDiffText, toLocaleCurrency, toQueryString } from '@utils/format'
-import { useGetPostQuery, useUpdateTradeStatusMutation } from '@apis'
-import { UserProfile, PriceOfferCard, PostFieldList, Dialog } from '@components'
+import {
+  useDeletePostMutation,
+  useGetPostQuery,
+  useUpdateTradeStatusMutation
+} from '@apis'
+import {
+  UserProfile,
+  PriceOfferCard,
+  PostFieldList,
+  Dialog,
+  CommonModal
+} from '@components'
 import { TRADE_STATUS } from '@constants'
-import { useAuth } from '@hooks'
+import { useAuth, useModal } from '@hooks'
 import type { TradeStatusCodes, TradeStatusType } from '@types'
 
 type Props = { postId: number }
@@ -22,14 +40,17 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({
 })
 
 const PostDetailPage = ({ postId }: Props): ReactElement => {
-  const postQuery = useGetPostQuery(postId)
-  const tradeStatusMutation = useUpdateTradeStatusMutation(postId)
+  const getPostQuery = useGetPostQuery(postId)
+  const updateTradeStatusMutation = useUpdateTradeStatusMutation(postId)
+  const deletePostMutation = useDeletePostMutation(postId)
+  const router = useRouter()
   const [tradeStatus, setTradeStatus] = useState<TradeStatusCodes>()
   const [isOpenDialog, setIsOpenDialog] = useState(false)
+  const { isOpen, openModal, closeModal } = useModal()
   const { user } = useAuth()
 
-  const isSeller = user.id === postQuery.data?.seller.id
-  const postImages = postQuery.data?.imageUrls.map((url, idx) => ({
+  const isSeller = user.id === getPostQuery.data?.seller.id
+  const postImages = getPostQuery.data?.imageUrls.map((url, idx) => ({
     id: idx,
     url
   }))
@@ -39,9 +60,15 @@ const PostDetailPage = ({ postId }: Props): ReactElement => {
 
     setTradeStatus(nextStatusCode)
 
-    await tradeStatusMutation.mutateAsync({
+    await updateTradeStatusMutation.mutateAsync({
       tradeStatus: nextStatusCode
     })
+  }
+
+  const handleClickDelete = async () => {
+    await deletePostMutation.mutateAsync()
+
+    router.replace('/')
   }
 
   const handleCloseDialog = () => {
@@ -52,98 +79,114 @@ const PostDetailPage = ({ postId }: Props): ReactElement => {
     setIsOpenDialog(true)
   }
 
-  useEffect(() => {
-    if (postQuery.data) {
-      setTradeStatus(postQuery.data.tradeStatus.code)
-    }
-  }, [postQuery.data])
-
   return (
-    <Layout>
-      <Main>
-        <div>
-          <Carousel images={postImages || []} isArrow name="post-carousel" />
-        </div>
-        <Content>
+    <>
+      <Layout>
+        <Main>
           <div>
-            <ProductCondition>
-              {isSeller ? (
-                <>
-                  <ProductConditionSelectBox
-                    colorType="dark"
-                    items={TRADE_STATUS}
-                    value={tradeStatus}
-                    onChange={handleChangeTradeStatus}
-                  />
-                  <MoreButtonWrapper>
-                    <IconButton
-                      icon="more"
-                      size={24}
-                      onClick={handleClickMore}
+            <Carousel images={postImages || []} isArrow name="post-carousel" />
+          </div>
+          <Content>
+            <div>
+              <ProductCondition>
+                {isSeller ? (
+                  <>
+                    <ProductConditionSelectBox
+                      colorType="dark"
+                      items={TRADE_STATUS}
+                      value={tradeStatus || getPostQuery.data?.tradeStatus.code}
+                      onChange={handleChangeTradeStatus}
                     />
-                    {isOpenDialog && (
-                      <Dialog
-                        dialogPositionStyle={{
-                          top: '34px',
-                          right: '0'
-                        }}
-                        onClose={handleCloseDialog}>
-                        <DialogButtonContainer>
-                          <Link
-                            href={`/post${toQueryString({
-                              postId
-                            })}`}>
-                            <DialogButton>수정하기</DialogButton>
-                          </Link>
-                          <DialogButton>삭제하기</DialogButton>
-                        </DialogButtonContainer>
-                      </Dialog>
-                    )}
-                  </MoreButtonWrapper>
-                </>
-              ) : (
-                <ProductConditionBadge>
-                  {postQuery.data?.tradeStatus.code}
-                </ProductConditionBadge>
-              )}
-            </ProductCondition>
-            <Text color="grayScale70" styleType="body02M" tag="p">
-              {postQuery.data?.category.name || ''}
-            </Text>
-            <PostName styleType="headline01B" tag="p">
-              {postQuery.data?.title || ''}
-            </PostName>
-            <Text styleType="display01B" tag="p">
-              {toLocaleCurrency(Number(postQuery.data?.price))}
-              <Text styleType="subtitle01M">원</Text>
-            </Text>
-          </div>
-          <Divider gap={16} />
-          <div>
-            <Text styleType="headline02B">상품 정보</Text>
-            <TransactionContainer>
-              <PostFieldList
-                date={getTimeDiffText(postQuery.data?.createdAt || '')}
-                location={postQuery.data?.location || ''}
-                productCondition={postQuery.data?.productCondition.name}
-                tradeType={postQuery.data?.tradeType.name}
-              />
-            </TransactionContainer>
-            <Description>{postQuery.data?.description}</Description>
-          </div>
-          <Divider gap={16} />
-          <UserProfile
-            image={postQuery.data?.seller.profileImageUrl}
-            level={postQuery.data?.seller.offerLevel || 0}
-            location={postQuery.data?.seller.nickname || ''}
-            nickName={postQuery.data?.seller.nickname || ''}
-            type="basic"
-          />
-        </Content>
-      </Main>
-      <MainDivider size="bold" />
-      <PriceOfferCard isSeller={isSeller} postId={postId} />
-    </Layout>
+                    <MoreButtonWrapper>
+                      <IconButton
+                        icon="more"
+                        size={24}
+                        onClick={handleClickMore}
+                      />
+                      {isOpenDialog && (
+                        <Dialog
+                          dialogPositionStyle={{
+                            top: '34px',
+                            right: '0'
+                          }}
+                          onClose={handleCloseDialog}>
+                          <DialogButtonContainer>
+                            <Link
+                              href={`/post${toQueryString({
+                                postId
+                              })}`}>
+                              <DialogButton>수정하기</DialogButton>
+                            </Link>
+                            <DialogButton onClick={openModal}>
+                              삭제하기
+                            </DialogButton>
+                          </DialogButtonContainer>
+                        </Dialog>
+                      )}
+                    </MoreButtonWrapper>
+                  </>
+                ) : (
+                  <ProductConditionBadge>
+                    {getPostQuery.data?.tradeStatus.code}
+                  </ProductConditionBadge>
+                )}
+              </ProductCondition>
+              <Text color="grayScale70" styleType="body02M" tag="p">
+                {getPostQuery.data?.category.name || ''}
+              </Text>
+              <PostName styleType="headline01B" tag="p">
+                {getPostQuery.data?.title || ''}
+              </PostName>
+              <Text styleType="display01B" tag="p">
+                {toLocaleCurrency(Number(getPostQuery.data?.price))}
+                <Text styleType="subtitle01M">원</Text>
+              </Text>
+            </div>
+            <Divider gap={16} />
+            <div>
+              <Text styleType="headline02B">상품 정보</Text>
+              <TransactionContainer>
+                <PostFieldList
+                  date={getTimeDiffText(getPostQuery.data?.createdAt || '')}
+                  location={getPostQuery.data?.location || ''}
+                  productCondition={getPostQuery.data?.productCondition.name}
+                  tradeType={getPostQuery.data?.tradeType.name}
+                />
+              </TransactionContainer>
+              <Description>{getPostQuery.data?.description}</Description>
+            </div>
+            <Divider gap={16} />
+            <UserProfile
+              image={getPostQuery.data?.seller.profileImageUrl}
+              level={getPostQuery.data?.seller.offerLevel || 0}
+              location={getPostQuery.data?.seller.nickname || ''}
+              nickName={getPostQuery.data?.seller.nickname || ''}
+              type="basic"
+            />
+          </Content>
+        </Main>
+        <MainDivider size="bold" />
+        <PriceOfferCard isSeller={isSeller} postId={postId} />
+      </Layout>
+      <CommonModal
+        buttons={[
+          <Button key="delete" size="large" onClick={handleClickDelete}>
+            삭제
+          </Button>,
+          <Button
+            key="cancel"
+            size="large"
+            styleType="ghost"
+            onClick={closeModal}>
+            취소
+          </Button>
+        ]}
+        description="삭제한 게시글은 복구할 수 없어요."
+        isOpen={isOpen}
+        title="게시글을 삭제하시겠어요?"
+        onClose={closeModal}
+      />
+    </>
   )
 }
 

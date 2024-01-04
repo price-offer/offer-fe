7437cd2ab3aa4bb5ecafd5f16419c9001ceb1175
sonsurim <surim014@naver.com>
2024-01-05 00:50:10 +0900

    add useUpdateLikeStatusMutation types

--- src/apis/like/queries.ts
+++ src/apis/like/queries.ts
@@ -1,6 +1,7 @@
+import type { DefaultError } from '@tanstack/react-query'
 import { useQuery, useMutation } from '@tanstack/react-query'
 import { getLikedPosts, updateLikeStatus } from './apis'
-import type { GetLikedPostsReq } from './types'
+import type { GetLikedPostsReq, UpdateLikeStatusReq } from './types'
 
 export const useGetLikedPostsQuery = (searchOptions: GetLikedPostsReq) =>
   useQuery({
@@ -9,6 +10,6 @@ export const useGetLikedPostsQuery = (searchOptions: GetLikedPostsReq) =>
   })
 
 export const useUpdateLikeStatusMutation = () =>
-  useMutation({
-    mutationFn: (postId: number) => updateLikeStatus(postId)
+  useMutation<null, DefaultError, UpdateLikeStatusReq>({
+    mutationFn: ({ postId }) => updateLikeStatus(postId)
   })

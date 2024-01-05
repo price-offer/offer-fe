03036e771cd49ab91636087016fc0c9f8abd6016
sonsurim <surim014@naver.com>
2024-01-06 01:26:36 +0900

    implement features of profile box

--- src/pages/shop/view/index.tsx
+++ src/pages/shop/view/index.tsx
@@ -1,25 +1,106 @@
 import { Divider } from '@offer-ui/react'
-import type { MouseEvent } from 'react'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { Styled } from './styled'
 import { pageTabs } from '../pageTabs'
-import { useGetProfileQuery } from '@apis'
-import { ProfileBox, Tabs } from '@components'
+import { VALID_NICKNAME_MESSAGE } from '@constants/message'
+import {
+  useCheckValidNicknameMutation,
+  useGetProfileQuery,
+  usePostUploadImageMutation,
+  useUpdateMyProfileMutation
+} from '@apis'
+import type { EditProfileForm } from '@components'
+import { EditProfileModal, ProfileBox, Tabs } from '@components'
+import { useModal } from '@hooks'
+import type { TradeActivityCodes } from '@types'
 import { isNumber } from '@utils'
+
+const initialEditProfileValidate = {
+  isSuccess: false,
+  message: ''
+}
 
 type ShopPageViewProps = {
   memberId: number | null
+  currentTab: TradeActivityCodes
 }
-export const ShopPageView = ({ memberId }: ShopPageViewProps) => {
-  const [pageIndex, setPageIndex] = useState<number>(0)
-  const profile = useGetProfileQuery(memberId)
-  const hasToken = !isNumber(memberId)
+export const ShopPageView = ({ memberId, currentTab }: ShopPageViewProps) => {
+  const [currentPage, setCurrentPage] = useState<TradeActivityCodes>(currentTab)
+  const [editProfileValidate, setEditProfileValidate] = useState(
+    initialEditProfileValidate
+  )
 
-  const handleTabClick = (
-    _: MouseEvent<HTMLDivElement>,
-    index: number
-  ): void => {
-    setPageIndex(index)
+  const profile = useGetProfileQuery(memberId)
+  const postUploadImage = usePostUploadImageMutation()
+  const checkValidNickname = useCheckValidNicknameMutation()
+  const updateMyProfile = useUpdateMyProfileMutation()
+
+  const hasToken = !isNumber(memberId)
+  const profileModal = useModal()
+  const router = useRouter()
+
+  const handleChangePage = (code: TradeActivityCodes) => (): void => {
+    router.push(`${router.pathname}?tab=${code}`)
+    setCurrentPage(code)
+  }
+
+  const handleValidateNickname = async (nickname: string) => {
+    if (nickname.length === 0) {
+      setEditProfileValidate({
+        isSuccess: false,
+        message: VALID_NICKNAME_MESSAGE.EMPTY_ERROR
+      })
+      return
+    }
+
+    if (nickname.length < 2) {
+      setEditProfileValidate({
+        isSuccess: false,
+        message: VALID_NICKNAME_MESSAGE.MIN_LENGTH_ERROR
+      })
+      return
+    }
+
+    const { duplicate } = await checkValidNickname.mutateAsync(nickname)
+
+    if (duplicate) {
+      setEditProfileValidate({
+        isSuccess: false,
+        message: VALID_NICKNAME_MESSAGE.DUPLICATED_ERROR
+      })
+    } else {
+      setEditProfileValidate({
+        isSuccess: true,
+        message: VALID_NICKNAME_MESSAGE.SUCCESS
+      })
+    }
+  }
+  const handleChangeProfileImage = async (image: EditProfileForm['image']) => {
+    if (!image.file) {
+      return image
+    }
+
+    const imageFormData = new FormData()
+    imageFormData.append('files', image.file)
+    const { imageUrl } = await postUploadImage.mutateAsync(imageFormData)
+
+    return { id: image.id, file: image.file, url: imageUrl }
+  }
+
+  const handleCloseEditProfileModal = () => {
+    setEditProfileValidate(initialEditProfileValidate)
+    profileModal.closeModal()
+  }
+  const handleConfirmEditProfile = async (profileForm: EditProfileForm) => {
+    await updateMyProfile.mutateAsync({
+      memberId: profile.data.id,
+      nickname: profileForm.nickname,
+      profileImageUrl: profileForm.image.url
+    })
+    await profile.refetch()
+
+    handleCloseEditProfileModal()
   }
 
   return (
@@ -31,11 +112,11 @@ export const ShopPageView = ({ memberId }: ShopPageViewProps) => {
           <Tabs.List>
             {pageTabs
               .filter(pageTab => (hasToken ? true : pageTab.tab.code !== 'buy'))
-              .map(({ tab }, index) => (
+              .map(({ tab }) => (
                 <Styled.Tab
                   key={`${tab.code}-tab`}
-                  isSelected={pageIndex === index}
-                  onClick={handleTabClick}>
+                  isSelected={currentPage === tab.code}
+                  onClick={handleChangePage(tab.code)}>
                   {tab.name}
                 </Styled.Tab>
               ))}
@@ -49,7 +130,11 @@ export const ShopPageView = ({ memberId }: ShopPageViewProps) => {
               .map(({ tab, panel }) => (
                 <Tabs.Panel key={`${tab.code}-panel`}>
                   <Styled.TabPanelContent>
-                    <ProfileBox {...profile.data} hasToken={hasToken} />
+                    <ProfileBox
+                      {...profile.data}
+                      hasToken={hasToken}
+                      onClickEditButton={profileModal.openModal}
+                    />
                     {panel({ hasToken, memberId })}
                   </Styled.TabPanelContent>
                 </Tabs.Panel>
@@ -58,6 +143,14 @@ export const ShopPageView = ({ memberId }: ShopPageViewProps) => {
         </Styled.Layout>
       </Tabs>
       <Divider />
+      <EditProfileModal
+        isOpen={profileModal.isOpen}
+        validate={editProfileValidate}
+        onChangeImage={handleChangeProfileImage}
+        onClose={handleCloseEditProfileModal}
+        onConfirm={handleConfirmEditProfile}
+        onValidateNickname={handleValidateNickname}
+      />
     </div>
   )
 }

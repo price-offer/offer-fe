5963d3accbca8083e0cf3a5912f61be09ad4fb28
shinhyojeong <hyojeonge0@naver.com>
2024-01-22 20:21:12 +0900

    refactor: 결과물 페이지 필터링 로직 복잡도 개선

--- src/components/result/FilterSelect/index.tsx
+++ src/components/result/FilterSelect/index.tsx
@@ -2,60 +2,48 @@ import type { ReactElement } from 'react'
 import { Styled } from './styled'
 import type { FilterSelectProps } from './types'
 import { PriceDialog } from '../PriceDialog'
-import { SORT_OPTIONS } from '@constants/app'
+import { SORT_OPTIONS, TRADE_TYPES } from '@constants'
+
 const FilterSelect = ({
-  categoryItems,
-  tradePeriodItems,
-  selectedCategoryValue,
-  selectedTradePeriodValue,
-  selectedSortPriceValue,
-  inputPrice,
-  applyPrice,
-  postSummariesLength,
-  handleSortPriceChange,
-  handleCategoryChange,
-  handleTradePeriodChange,
-  handlePriceInputChange,
-  handlePriceApplyClick
+  categories,
+  handleChangeFilters,
+  filters,
+  postCount = 0
 }: FilterSelectProps): ReactElement => {
   return (
-    <>
-      <Styled.SelectWrapper>
-        <Styled.LeftSelectWrapper>
-          <Styled.CategorySelect
-            colorType="dark"
-            items={categoryItems}
-            placeholder={selectedCategoryValue}
-            value={selectedCategoryValue}
-            onChange={handleCategoryChange}
-          />
-          <Styled.TradePeriodSelect
-            colorType="light"
-            items={tradePeriodItems}
-            placeholder="거래방식"
-            value={selectedTradePeriodValue}
-            onChange={handleTradePeriodChange}
-          />
-          <PriceDialog
-            applyPrice={applyPrice}
-            handlePriceApplyClick={handlePriceApplyClick}
-            handlePriceInputChange={handlePriceInputChange}
-            inputPrice={inputPrice}
-          />
-        </Styled.LeftSelectWrapper>
-        <Styled.RightSelectWrapper>
-          <Styled.ProductCount>
-            전체 {postSummariesLength && postSummariesLength}개
-          </Styled.ProductCount>
-          <Styled.PriceFilterSelect
-            colorType="none"
-            items={SORT_OPTIONS}
-            value={selectedSortPriceValue}
-            onChange={handleSortPriceChange}
-          />
-        </Styled.RightSelectWrapper>
-      </Styled.SelectWrapper>
-    </>
+    <Styled.SelectWrapper>
+      <Styled.LeftSelectWrapper>
+        <Styled.CategorySelect
+          colorType="dark"
+          items={categories}
+          placeholder="전체"
+          value={filters?.category || 'ALL'}
+          onChange={({ code }) => handleChangeFilters('category', code)}
+        />
+        <Styled.TradePeriodSelect
+          colorType="light"
+          items={TRADE_TYPES}
+          placeholder="거래방식"
+          value={filters?.tradeType}
+          onChange={({ code }) => handleChangeFilters('tradeType', code)}
+        />
+        <PriceDialog
+          priceRange={filters.priceRange}
+          onClickApply={priceRange => {
+            handleChangeFilters('priceRange', priceRange)
+          }}
+        />
+      </Styled.LeftSelectWrapper>
+      <Styled.RightSelectWrapper>
+        <Styled.ProductCount>전체 {postCount}개</Styled.ProductCount>
+        <Styled.PriceFilterSelect
+          colorType="none"
+          items={SORT_OPTIONS}
+          value={filters.sort}
+          onChange={({ code }) => handleChangeFilters('sort', code)}
+        />
+      </Styled.RightSelectWrapper>
+    </Styled.SelectWrapper>
   )
 }
 export { FilterSelect, FilterSelectProps }
--- src/components/result/FilterSelect/types.ts
+++ src/components/result/FilterSelect/types.ts
@@ -1,26 +1,22 @@
-import type { SelectOnChangeHandler } from '@offer-ui/react'
-import type { ChangeEventHandler } from 'react'
-import type { GetPostsRes } from '@apis'
-import type { ApplyPriceType } from '@hooks'
-import type { TradeTypes, SortOptionCodes, TradeTypeCodes } from '@types'
+import type { Category, SortOptionCodes, TradeTypeCodes } from '@types'
+
+export type Filters = {
+  sort: SortOptionCodes
+  category?: string
+  tradeType?: TradeTypeCodes
+  priceRange: {
+    min: number
+    max?: number
+  }
+}
+export type HandleChangeFilter = (
+  name: KeyOf<Filters>,
+  value: ValueOf<Filters>
+) => void
 
 export type FilterSelectProps = {
-  applyPrice: ApplyPriceType
-  inputPrice: ApplyPriceType
-  postData?: GetPostsRes[]
-  categoryItems: {
-    code: string
-    name: string
-    selected: boolean
-  }[]
-  tradePeriodItems: TradeTypes
-  selectedCategoryValue: string
-  selectedTradePeriodValue: TradeTypeCodes | ''
-  selectedSortPriceValue: SortOptionCodes
-  postSummariesLength: number
-  handleSortPriceChange: SelectOnChangeHandler
-  handleCategoryChange: SelectOnChangeHandler
-  handleTradePeriodChange: SelectOnChangeHandler
-  handlePriceInputChange: ChangeEventHandler
-  handlePriceApplyClick(): void
+  categories: Pick<Category, 'code' | 'name'>[]
+  postCount?: number
+  filters: Filters
+  handleChangeFilters: HandleChangeFilter
 }
